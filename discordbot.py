c803de78ed4c04a536e47f11562ef1b7ff3af9a9
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-16 22:13:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -55,17 +55,17 @@ async def on_voice_state_update(member, before, after):
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
     if before.channel != after.channel:
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(876803479006486550)
+        botRoom = client.get_channel('876803479006486550')
  
         # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-        announceChannelIds = [876803479006486551, 876803479006486551]
+        announceChannelIds = ['876803479006486551', '876803479006486551']
  
         # 退室通知
         if before.channel is not None and before.channel.id in announceChannelIds:
-            await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けました！")
+            await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けましたけど...")
         # 入室通知
         if after.channel is not None and after.channel.id in announceChannelIds:
-            await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しました！")
+            await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しましたけど...")
 
             
 token = getenv('DISCORD_BOT_TOKEN')
