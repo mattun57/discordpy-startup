dafc5f3ad9cc7ea6f0c02290b0db4f0a17f5b097
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-16 18:54:30 +0900

    [add] target method

--- discordbot.py
+++ discordbot.py
@@ -13,8 +13,40 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def shibuya(ctx):
+    await ctx.send('渋谷は苦手ですけど...')
+    
+
+@bot.command()
+async def mori(ctx):
+    await ctx.send('都会の公園を走るリスさんのように♪')
+    
+    
+    
+@bot.command()
+async def mattun(ctx):
+    await ctx.send('プロデューサーさん...ありがとうございます...')
+    
+    
+@bot.command()
+async def miyagawa(ctx):
+    await ctx.send('ひっ...')
+    
+    
+@client.event
+async def on_voice_state_update(member, before, after):
+ 
+    # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
+    if before.channel != after.channel:
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(aaaaaaaaaaaaaaaaaa)
+ 
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        announceChannelIds = [bbbbbbbbbbbbbbbbbb]
+
+        # 入室通知
+        if after.channel is not None and after.channel.id in announceChannelIds:
+            await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が入ってきましたけど...")
 
 
 token = getenv('DISCORD_BOT_TOKEN')
