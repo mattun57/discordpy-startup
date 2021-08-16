9982083932752022062e3b39485e8154845eba53
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-16 21:45:43 +0900

    [modify] チャンネルID追加

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_command_error(ctx, error):
 async def shibuya(ctx):
     await ctx.send('渋谷は苦手ですけど...')
     
-'''
+
 @bot.command()
 async def mori(ctx):
     await ctx.send('都会の公園を走るリスさんのように♪')
@@ -39,15 +39,15 @@ async def on_voice_state_update(member, before, after):
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
     if before.channel != after.channel:
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(aaaaaaaaaaaaaaaaaa)
+        botRoom = client.get_channel(876803479006486550)
  
         # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-        announceChannelIds = [bbbbbbbbbbbbbbbbbb]
+        announceChannelIds = [876803479006486551]
 
         # 入室通知
         if after.channel is not None and after.channel.id in announceChannelIds:
             await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が入ってきましたけど...")
 
-'''
+
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
