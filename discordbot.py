9b76a5de15b923688885630d18e8441c5567155c
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-16 22:28:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -55,10 +55,10 @@ async def on_voice_state_update(member, before, after):
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
     if before.channel != after.channel:
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(876803479006486550)
+        botRoom = client.get_channel("876803479006486550")
  
         # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-        announceChannelIds = [876803479006486551, 876803479006486551]
+        announceChannelIds = ["876803479006486551", "876803479006486551"]
  
         # 退室通知
         if before.channel is not None and before.channel.id in announceChannelIds:
