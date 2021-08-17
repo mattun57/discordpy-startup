8783a3a2370561d4cfce8f7b02572b369a04f3d1
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-17 14:19:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,7 +34,7 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     if member.guild.id == 600996774336790538:
         text_ch = 600996774336790539
-	botRoom = client.get_channel(600996774336790539)
+        botRoom = client.get_channel(600996774336790539)
         if before.channel is None:
             msg = f'{member.name} さんが {after.channel.name} に参加しましたけど...'
             await botRoom.send(msg)
