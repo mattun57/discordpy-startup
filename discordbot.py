f15a502c0198cda40c6760f93b289f14449b142d
まっつん <48915140+mattun57@users.noreply.github.com>
2021-08-17 11:27:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from os import getenv
 import traceback
 import discord
+import asyncio
 
 bot = commands.Bot(command_prefix='/')
 client = discord.Client()
@@ -20,7 +21,7 @@ async def shibuya(ctx):
 
 @bot.command()
 async def mori(ctx):
-    await ctx.send('都会の公園を走るリスさんのように♪')
+    await ctx.send('こんにちは、お元気にしてますか？私は今、もりのくにで暮らしています。')
        
     
 @bot.command()
@@ -44,56 +45,47 @@ async def yes(ctx):
     
     
 @bot.command()
+async def wait(ctx):
+    await ctx.send('ちょっと待ってくださいぃ...')
+    
+    
+@bot.command()
 async def ass(ctx):
     await ctx.send('ケツの穴臭すぎですけど...')
     
     
 @bot.command()
-async def test(ctx):
-    await ctx.send('テストですけど...')
+async def time(ctx):
+    await ctx.send('https://www.time-j.net/WorldTime/Country/JP')
     
     
 @bot.command()
-async def he(ctx):
-    await ctx.send('オナラですけど...')
+async def yeah(ctx):
+    await ctx.send("Yeah! That's exactly what l've been wanting BOOMERANG!!!")
     
     
 @bot.command()
 async def dare(ctx):
     await ctx.send('森久保ですけど...')
     
-  
-'''
-# チャンネル入退室時の通知処理
+
 @client.event
 async def on_voice_state_update(member, before, after):
  
-    # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-    botRoom = client.get_channel(600996774336790539)
-
-    # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-    announceChannelIds = [600996774336790541]
-
-    # 退室通知
-    if before.channel is not None and before.channel.id in announceChannelIds:
-        await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けましたけど...")
-    # 入室通知
-    if after.channel is not None and after.channel.id in announceChannelIds:
-        await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しましたけど...")
-'''
-
-@client.event
-async def on_voice_state_update(member,before,after):
     if before.channel != after.channel:
-        # before.channelとafter.channelが異なるなら入退室
-        if after.channel and len(after.channel.members) == 1:
-            # もし、ボイスチャットが開始されたら
-            send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しましたけど...")
+        # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
+        botRoom = client.get_channel(600996774336790539)
+        # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
+        announceChannelIds = [600996774336790541]
 
-        if before.channel and len(before.channel.members) == 0:
-            # もし、ボイスチャットが終了したら
-            send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けましたけど...")
-            
+        # 退室通知
+        if before.channel is not None and before.channel.id in announceChannelIds:
+            await botRoom.send("**" + before.channel.name + "** から、__" + member.name + "__  が抜けましたけど...")
+        # 入室通知
+        if after.channel is not None and after.channel.id in announceChannelIds:
+            await botRoom.send("**" + after.channel.name + "** に、__" + member.name + "__  が参加しましたけど...")
+
+           
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
 client.run(token)
