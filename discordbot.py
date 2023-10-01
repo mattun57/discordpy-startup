f09cf233f9ca6d7f1f1fdb8d2d7d494776d08669
Ryusei Matsumoto <48915140+mattun57@users.noreply.github.com>
2023-10-01 09:03:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_voice_state_update(member, before, after):
     if member.guild.id == 'サーバーIDをここに記入（クォート不要）':
         txt_room = client.get_channel('テキストチャンネルIDをここに記入（クォート不要）')
         if before.channel is None:
-            msg = f'**{member.name}**さんが **{member.guild.name}** に参加しましたけど...'
+            msg = f'**{member.name}**さんが **{member.guild.name}** に参加しました。'
             await txtRoom.send(msg)
 
 
@@ -26,8 +26,8 @@ async def on_message(message):
     if message.content.startswith("time"):
         await message.channel.send("https://www.time-j.net/WorldTime/Country/JP")
 
-    if message.content.startswith("のー"):
-        msg = f'**{message.member.name}**さんは今日むーりぃーみたいですけど...'
+    if message.content.startswith("no"):
+        msg = f'**{message.member.name}**さんは今日参加できないようです。'
         await message.channel.send(msg)
 
 token = getenv('DISCORD_BOT_TOKEN')
